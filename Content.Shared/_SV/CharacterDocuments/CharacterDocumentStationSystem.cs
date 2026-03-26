c7dc75ed618c1de2a5ad3d0170ee437e1439eb05
ReboundQ3 <22770594+ReboundQ3@users.noreply.github.com>
2026-03-26 18:03:09 +0100

    Part 5.1

--- Content.Shared/_SV/CharacterDocuments/CharacterDocumentStationSystem.cs
+++ Content.Shared/_SV/CharacterDocuments/CharacterDocumentStationSystem.cs
@@ -16,7 +16,6 @@ public sealed partial class CharacterDocumentStationSystem : EntitySystem
     public void OnPlayerSpawn(PlayerSpawnCompleteEvent args)
     {
         if (!HasComp<CharacterDocumentStationComponent>(args.Station))
-            Log.Error("Station did not have the CharacterDocumentStationComponent");
-        return;
+            return;
     }
 }
