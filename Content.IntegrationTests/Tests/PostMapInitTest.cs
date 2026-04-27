0c5924a0f46d7948666633d16cefe04c50cfc25d
Sophia12434 <sanesthetheskeleton@gmail.com>
2026-04-27 19:18:20 +0200

    Couple fixes
    fixed names for the file names + whitelisted them For DNM

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -130,7 +130,8 @@ namespace Content.IntegrationTests.Tests
             {"/Maps/Shuttles/ShuttleEvent/instigator.yml", ["ShuttleGunFriendship"]},
             {"/Maps/_LateStation/shuttles/avenger.yml", ["ShuttleGunSvalinnMachineGun", "ShuttleGunDuster"]},
             {"/Maps/_SV/cluster.yml", ["RubberStampMime"]},
-            {"/Maps/_SV/Shuttles/Syndicate/big/MambaFixed.yml", ["RubberStampSyndicate", "ShuttleGunDuster", "ShuttleGunDusterCircuitboard"]}, //SV Custom Syndi shuttle
+            {"/Maps/_SV/Shuttles/Syndicate/big/Mamba.yml", ["RubberStampSyndicate", "ShuttleGunDuster", "ShuttleGunDusterCircuitboard"]}, //SV Custom Syndi shuttle
+            {"/Maps/_SV/Shuttles/Syndicate/big/CopperHead.yml", ["RubberStampSyndicate", "ShuttleGunDuster", "ShuttleGunDusterCircuitboard"]}, //SV Custom Syndi shuttle
         };
 
         /// <summary>
