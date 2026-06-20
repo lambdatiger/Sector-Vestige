ec0386b49d6d3084a49a1fd832a2e46d4d5d22e6
lambdatiger <spam.stnuocca.sl@gmail.com>
2026-06-19 22:13:35 -0500

    renamed event maps and removed smaller event grids.

--- Content.IntegrationTests/Tests/Power/StationPowerTests.cs
+++ Content.IntegrationTests/Tests/Power/StationPowerTests.cs
@@ -55,7 +55,7 @@ public sealed class StationPowerTests : GameTest
             "Cluster", // Sector Vestige, remade by Rebound
             "Packed_SM", // Sector Vestige, edits by Lambda.Tiger
             "Amber", // Sector Vestige, edits by Lambda.Tiger
-            "AmberFucked", // Sector Vestige, event map, edits by Lambda.Tiger
+            "AmberEvent", // Sector Vestige, event map, edits by Lambda.Tiger
     ];
 
     public override PoolSettings PoolSettings => new ()
